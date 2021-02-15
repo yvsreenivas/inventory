c37b844a77bbb45381f19e185a294dce9f04f803
yvsreenivas <yvsreenivas@yahoo.co.in>
2021-02-15 11:55:44 +0530

    Time zone problem

--- aistores/settings.py
+++ aistores/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import pytz
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'ASIA/KOLKATA'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
