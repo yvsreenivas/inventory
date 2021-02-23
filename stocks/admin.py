f826cdf4b8bce67dce55d377577dcc88fc0e7b6c
yvsreenivas <yvsreenivas@yahoo.co.in>
2021-02-23 15:41:45 +0530

    added partno to admin

--- stocks/admin.py
+++ stocks/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 class StockCreateAdmin(admin.ModelAdmin):
-   list_display = ['category', 'item_name', 'quantity']
+   list_display = ['category', 'part_no', 'item_name', 'quantity']
    form = StockCreateForm
    list_filter = ['category']
    search_fields = ['category', 'item_name']
