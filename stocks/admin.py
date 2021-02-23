6992748d63bbcbb2b7c2e624a0af8ca0400c176d
yvsreenivas <yvsreenivas@yahoo.co.in>
2021-02-23 16:19:59 +0530

    Added category list filter

--- stocks/admin.py
+++ stocks/admin.py
@@ -4,9 +4,9 @@ from .models import *
 
 
 class StockCreateAdmin(admin.ModelAdmin):
-   list_display = ['id','subcategory', 'part_no', 'item_name', 'quantity']
+   list_display = ['id','category','subcategory', 'part_no', 'item_name', 'quantity']
    form = StockCreateForm
-   list_filter = ['subcategory']
+   list_filter = ['category', 'subcategory']
    search_fields = ['category', 'item_name']
 
 
