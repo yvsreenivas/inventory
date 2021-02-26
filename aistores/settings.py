9f91bc385c897daa54965c879484e4b494bee165
yvsreenivas <yvsreenivas@yahoo.co.in>
2021-02-26 12:12:39 +0530

    Added FIXTURE_DIRS to settings.py

--- aistores/settings.py
+++ aistores/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
+FIXTURE_DIRS = os.path.join(BASE_DIR,'stocks/fixtures')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
